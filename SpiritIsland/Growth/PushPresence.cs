370a41a26e44958eb4a726ee8485ae6b8327931b
Dean Rettig <rettigcd@gmail.com>
2021-05-08 13:28:08 -0400

    refactoring GatherPresence to have from and to both be spaces (for uniformity)

--- SpiritIsland/Growth/PushPresence.cs
+++ SpiritIsland/Growth/PushPresence.cs
@@ -5,15 +5,14 @@ namespace SpiritIsland {
 	public class PushPresence : GrowthAction {
 
 		public Space From { get; }
-		public string To {get; set; }
+		public Space To {get; set;}
 
 		public PushPresence(Spirit spirit, Space from):base(spirit){
 			this.From = from; 
 			Options = from.SpacesExactly(1)
-				.Where(spirit.Presence.Contains)
 				.ToArray();
 			if(Options.Length == 1)
-				To = Options[0].Label;
+				To = Options[0];
 		}
 
 		public Space[] Options { get; }
@@ -21,9 +20,8 @@ namespace SpiritIsland {
 		public override void Apply() {
 			if(To==null)
 				throw new InvalidOperationException("Destination Presence land not specified");
-			var toSpace = From.SpacesExactly(1).First(x=>x.Label==To);
 			spirit.Presence.Remove(From);
-			spirit.Presence.Add(toSpace);
+			spirit.Presence.Add(To);
 		}
 
 		public class Resolve : IResolver {
@@ -37,7 +35,9 @@ namespace SpiritIsland {
 				var action = growthOption.GrowthActions
 					.OfType<PushPresence>()
 					.VerboseSingle(a=>a.From.Label == from);
-				action.To = to;
+
+				action.To = action.Options.First(x=>x.Label==to);
+
 			}
 		}
 
