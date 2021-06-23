20e9daf179c4bd36d26e1e7adb9515945fde340e
Dean Rettig <rettigcd@gmail.com>
2021-06-23 09:49:25 -0400

    Created Command-Line game project

---
+++ SpiritIslandCmd/Program.cs
@@ -0,0 +1,38 @@
+﻿using SpiritIsland;
+using System;
+
+namespace SpiritIslandCmd {
+
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+
+			var phase = Phase.Growth;
+
+			var spirit = new RiverSurges();
+			var board = Board.BuildBoardA();
+			var gameState = new GameState(spirit){ Island = new Island(board)};
+
+			while(phase != Phase.Done){
+				Console.Write("\r\nSI > ");
+				string cmd = Console.ReadLine().ToLower();
+
+				switch(cmd){
+					case "spirit":
+						Console.WriteLine($"Spirit: {spirit.Text} ET:{spirit.EnergyPerTurn} CT:{spirit.NumberOfCardsPlayablePerTurn}" );
+						break;
+					case "board":
+						Console.WriteLine($"Board: {board[0].Label}" );
+						break;
+				}
+
+			}
+
+
+		}
+
+	}
+
+	public enum Phase { Growth, Fast, Invader, Slow, TimePasses, Done }
+
+}
