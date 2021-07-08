6a0779fd986d1e83721b2fa2409101f28e6c9811
Dean Rettig <rettigcd@gmail.com>
2021-07-08 15:03:05 -0400

    async Natures Resilience

--- SpiritIsland/Base/Minor/NaturesResilience.cs
+++ SpiritIsland/Base/Minor/NaturesResilience.cs
@@ -1,44 +1,33 @@
-﻿using SpiritIsland.Core;
+﻿using System.Threading.Tasks;
+using SpiritIsland.Core;
 
 namespace SpiritIsland.Base {
 
 	[MinorCard(NaturesResilience.Name,1,Speed.Fast,Element.Earth,Element.Plant,Element.Animal)]
-	public class NaturesResilience : TargetSpaceAction {
+	public class NaturesResilience : BaseAction {
 
 		public const string Name = "Nature's Resilience";
 		const string DefendKey = "Defend 6";
 		const string RemoveBlightKey = "Remove Blight";
 
-		readonly bool canRemoveBlight;
-		Space space;
+		public NaturesResilience(Spirit self,GameState gameState):base(gameState) {
+			_ = Act(self);
+		}
 
-		public NaturesResilience(Spirit self,GameState gameState):base(self,gameState,1,From.SacredSite){
+		async Task Act(Spirit self){
 			// if 2 water, you may INSTEAD remove 1 blight
-			canRemoveBlight = self.Elements(Element.Water)>=2;
-		}
 
-		protected override void SelectSpace(Space space){
-			this.space = space;
+			var target = await engine.SelectSpace("",self.SacredSites.Range(1));
 
-			if(canRemoveBlight)
-				engine.decisions.Push( new SelectText( engine, 
-					SelectEffect,
-					DefendKey, RemoveBlightKey 
-				));
-			else
-				Defend6();
+			bool removeBlight = self.Elements(Element.Water)>=2
+				&& await engine.SelectText("Select option",DefendKey, RemoveBlightKey) == RemoveBlightKey;
+
+			if(removeBlight)
+				gameState.RemoveBlight(target);
+			else 
+				gameState.Defend(target,6);
 
 		}
-
-		void SelectEffect(string option,ActionEngine engine){
-			switch(option){
-				case DefendKey:	      Defend6();       break;
-				case RemoveBlightKey: RemoveBlight(); break;
-			}
-		}
-
-		void Defend6()=>gameState.Defend(space,6);
-		void RemoveBlight()=>gameState.RemoveBlight(space);
 
 	}
 }
