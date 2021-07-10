3a4b83d19168b8dd5025ec7bfdcbb72e13f0e4d7
Dean Rettig <rettigcd@gmail.com>
2021-07-10 07:02:41 -0400

    async EncompasingWard * delete TargetSpiritAction.cs

--- SpiritIsland/Base/Minor/EncompassingWard.cs
+++ SpiritIsland/Base/Minor/EncompassingWard.cs
@@ -1,16 +1,22 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using SpiritIsland.Core;
 
 namespace SpiritIsland.Base {
 
 
 	[MinorCard(EncompassingWard.Name,1,Speed.Fast,Element.Sun,Element.Water,Element.Earth)]
-	public class EncompassingWard : TargetSpiritAction {
+	public class EncompassingWard : BaseAction {
 
 		public const string Name = "Encompassing Ward";
 
-		public EncompassingWard(Spirit self,GameState gs):base(self,gs){}
-		protected override void SelectSpirit(Spirit spirit){
+		public EncompassingWard(Spirit _,GameState gs):base(gs){
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+			Act();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+		}
+		async Task Act(){
+			var spirit = await engine.SelectSpirit();
 			// defend 2 in every land where spirit has presence
 			foreach(var space in spirit.Presence.Distinct())
 				gameState.Defend(space,2);
