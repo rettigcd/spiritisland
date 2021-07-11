e7ffab739121e84c5a191a8776af3ebde579fd1e
Dean Rettig <rettigcd@gmail.com>
2021-07-11 09:44:13 -0400

    finished Lightning power progression cards

--- SpiritIsland/Base/Minor/EntrancingApparitions.cs
+++ SpiritIsland/Base/Minor/EntrancingApparitions.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using SpiritIsland.Core;
 
 namespace SpiritIsland.Base {
 
-	[Core.MinorCard("Entrancing Apparitions",1,Speed.Fast,Element.Moon,Element.Air,Element.Water)]
 	class EntrancingApparitions {
-		// range 1
-		// defend 2
-		// if no invaders are present, gather 2 explorers
+
+		[MinorCard("Entrancing Apparitions",1,Speed.Fast,Element.Moon,Element.Air,Element.Water)]
+		static public async Task Act(ActionEngine engine){
+			var (spirit,gs) = engine;
+			var target = await engine.SelectSpace("select target",spirit.Presence.Range(1));
+			// defend 2
+			gs.Defend(target,2);
+
+			// if no invaders are present, gather 2 explorers
+			if(gs.HasInvaders(target)) return;
+
+			int remaining = 2;
+			var spacesWithExplorers = target.Neighbors.Where(n=>gs.InvadersOn(n).HasExplorer).ToArray();
+			while(remaining>0 && spacesWithExplorers.Length>0){
+				var source = await engine.SelectSpace("pull explorer from",spacesWithExplorers,true);
+				if(source==null) break;
+				gs.Adjust(target,Invader.Explorer,1);
+				gs.Adjust(source,Invader.Explorer,-1);
+				--remaining;
+				spacesWithExplorers = target.Neighbors.Where(n=>gs.InvadersOn(n).HasExplorer).ToArray();
+			}
+
+		}
+
 	}
 }
--- SpiritIsland/Base/Minor/PurifyingFlame.cs
+++ SpiritIsland/Base/Minor/PurifyingFlame.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using SpiritIsland.Core;
 
 namespace SpiritIsland.Base {
 
-	[Core.MinorCard("Purifying Flame",1,Speed.Slow,Element.Sun,Element.Fire,Element.Air,Element.Plant)]
 	class PurifyingFlame {
-		// range 1 from SS
-		// 1 damage per blight
-		// if target land is M/S, you may INSTEAD remove 1 blight
+
+		[MinorCard("Purifying Flame",1,Speed.Slow,Element.Sun,Element.Fire,Element.Air,Element.Plant)]
+		static public async Task Act(ActionEngine engine){
+			var (spirit,gameState) = engine;
+
+			static bool CanRemoveBlight(Space space) => space.Terrain.IsIn(Terrain.Mountain,Terrain.Sand);
+			bool CanDamageOrRemoveBlight(Space space) => gameState.HasBlight(space)
+				&& (CanRemoveBlight(space) || gameState.HasInvaders(space));
+
+			// range 1 from SS
+			var target = await engine.SelectSpace("Select target"
+				,spirit.SacredSites.Range(1).Where(CanDamageOrRemoveBlight)
+			); // !!! there might be 0 spaces that qualify
+
+			async Task<bool> UserSelectsDamage(){
+				const string damageInvaderKey = "damageInvaders";
+				return damageInvaderKey == await engine.SelectText("Select option",damageInvaderKey,"remove blight");
+			}
+
+			// on target, spirit should be able to do one or both
+			bool doDamage = gameState.HasInvaders(target) && (!CanRemoveBlight(target) || await UserSelectsDamage());
+			if(doDamage)
+				// 1 damage per blight
+				gameState.DamageInvaders(target,gameState.GetBlightOnSpace(target));
+			else
+				// if target land is M/S, you may INSTEAD remove 1 blight
+				gameState.AddBlight(target,-1);
+
+		}
+
 	}
+
 }
