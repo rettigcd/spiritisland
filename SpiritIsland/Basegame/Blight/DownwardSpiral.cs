d1f90bc5323502afdabaab57edc622042900baf9
Dean Rettig <rettigcd@gmail.com>
2021-08-04 07:45:40 -0400

    stubbed out blight cards

---
+++ SpiritIsland/Basegame/Blight/DownwardSpiral.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiritIsland.Basegame.Blight {
+
+	class DownwardSpiral {
+		// Spirit destorys a presence.
+		// +5 blight
+	}
+
+}
---
+++ SpiritIsland/Basegame/Blight/MemoryFadesToDust.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiritIsland.Basegame.Blight {
+	class MemoryFadesToDust {
+		// Forgets a Power or destorys a presence
+		// +4 more blight
+	}
+}
