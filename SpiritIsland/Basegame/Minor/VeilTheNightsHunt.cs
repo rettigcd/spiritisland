2840eb3ee8c3065fd046c7e0c15f4b5ad1308707
Dean Rettig <rettigcd@gmail.com>
2021-08-13 08:38:13 -0400

    Veil the nights hunt

--- SpiritIsland/Basegame/Minor/VeilTheNightsHunt.cs
+++ SpiritIsland/Basegame/Minor/VeilTheNightsHunt.cs
@@ -8,9 +8,18 @@ namespace SpiritIsland.Basegame {
 		[MinorCard( "Veil the Night's Hunt", 1, Speed.Fast, Element.Moon, Element.Air, Element.Animal)]
 		[FromPresence( 2, Filter.Dahan )]
 		static public async Task Act( ActionEngine engine, Space target ) {
-			// each dahan deals 1 damage to a different invader
-			// - or -
-			// push up to 3 dahan
+
+			int dahanCount = engine.GameState.GetDahanOnSpace(target);
+			string damageInvadersText = $"{dahanCount} damage to invaders";
+			bool damageInvaders = engine.GameState.HasInvaders(target)
+				&& await engine.SelectText("Select card option", damageInvadersText, "push up to 3 dahan") == damageInvadersText;
+
+			if(damageInvaders)
+				// each dahan deals 1 damage to a different invader
+				engine.GameState.DamageInvaders(target,dahanCount);
+			else
+				// push up to 3 dahan
+				await engine.PushUpToNDahan(target,3);
 		}
 
 	}
