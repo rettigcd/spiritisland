e6f7a8b0a485bef41e560dbfaf9365488b3b0af9
Dean Rettig <rettigcd@gmail.com>
2021-08-26 06:42:43 -0400

    Shuffle file locations

--- SpiritIsland/Core/Extensions/IEnumerableExtensions.cs
+++ SpiritIsland/Core/Extensions/IEnumerableExtensions.cs
@@ -37,6 +37,13 @@ namespace SpiritIsland {
 			}
 		}
 
+		// shorter syntax:
+		// space.Terrain.IsIn(Terrain.Wetland,Terrain.Sand)
+		// vs.
+		// new Terraion[]{Terrain.Wetland,Terrain.Sand}.Contains(space.Terrain);
+		static public bool IsIn<T>( this T needle, params T[] haystack ) where T : Enum
+			=> haystack.Contains( needle );
+
 	}
 
 }
