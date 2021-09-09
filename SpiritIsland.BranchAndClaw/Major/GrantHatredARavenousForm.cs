8fe8453ba05856d43265937cb76295e63b4e8a06
Dean Rettig <rettigcd@gmail.com>
2021-09-09 18:32:00 -0400

    Grant Hatred a Ravenous Form

--- SpiritIsland.BranchAndClaw/Major/GrantHatredARavenousForm.cs
+++ SpiritIsland.BranchAndClaw/Major/GrantHatredARavenousForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 
 namespace SpiritIsland.BranchAndClaw {
 
@@ -6,12 +7,32 @@ namespace SpiritIsland.BranchAndClaw {
 
 		[MajorCard( "Grant Hatred a Ravenous Form", 4, Speed.Slow, Element.Moon, Element.Fire )]
 		[FromPresence( 1 )]
-		static public Task ActAsync( TargetSpaceCtx ctx ) {
-			// for each strife or blight in target land, 1 fear and 2 damage.
-			// if this destorys all invaders in target land, add 1 beast.
+		static public async Task ActAsync( TargetSpaceCtx ctx ) {
 
-			// if you have 4 moon, 2 fire: add 1 strife in up to 3 adjacent lands.
-			return Task.CompletedTask;
+			bool hasInvaders = ctx.HasInvaders;
+
+			// for each strife or blight in target land, 1 fear and 2 damage.
+			int count = ctx.Tokens.Keys.OfType<StrifedInvader>().Sum(x=>x.StrifeCount * ctx.Tokens[x])
+				+ ctx.Tokens.Blight.Count;
+			ctx.AddFear( count );
+			await ctx.DamageInvaders( count * 2 );
+
+			// if this destorys all invaders in target land, add 1 beast.
+			if(hasInvaders && !ctx.HasInvaders)
+				ctx.Tokens.Beasts().Count++;
+
+			// if you have 4 moon, 2 fire
+			if(ctx.Self.Elements.Contains("4 moon,2 fire" )) {
+				// add 1 strife in up to 3 adjacent lands.
+				TokenCountDictionary[] tokenSpaces = ctx.PowerAdjacents()
+					.Select(x=>ctx.GameState.Tokens[x])
+					.Where(tokens=> tokens.HasInvaders())
+					.Take(3)
+					.ToArray();
+
+				foreach(var tokens in tokenSpaces)
+					await ctx.Self.SelectInvader_ToStrife( tokens );
+			}
 		}
 
 	}
