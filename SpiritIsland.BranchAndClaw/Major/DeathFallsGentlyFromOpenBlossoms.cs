b8e2f7847f01eb5c85477c78bdc17a8fccdc4d15
Dean Rettig <rettigcd@gmail.com>
2021-09-09 18:11:15 -0400

    Death Falls Gently

--- SpiritIsland.BranchAndClaw/Major/DeathFallsGentlyFromOpenBlossoms.cs
+++ SpiritIsland.BranchAndClaw/Major/DeathFallsGentlyFromOpenBlossoms.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 
 namespace SpiritIsland.BranchAndClaw {
 
@@ -6,10 +7,22 @@ namespace SpiritIsland.BranchAndClaw {
 
 		[MajorCard("Death Falls Gently from Open Blossoms",4, Speed.Slow, Element.Moon,Element.Air,Element.Plant)]
 		[FromPresence(3,Target.JungleOrSand)]
-		static public Task ActAsync(TargetSpaceCtx ctx ) {
-			// 4 damage.  If any invaders remain, add 1 disease
-			// if 3 air and 3 plant:  3 fear.  Add 1 disease to 2 adjacent lands with invaders.
-			return Task.CompletedTask;
+		static public async Task ActAsync(TargetSpaceCtx ctx ) {
+			// 4 damage.
+			await ctx.DamageInvaders(4);
+			// If any invaders remain, add 1 disease
+			if( ctx.Tokens.Invaders().Any())
+				ctx.Tokens.Disease().Count++;
+
+			// if 3 air and 3 plant:  
+			if( ctx.Self.Elements.Contains("3 air,3 plant" )) {
+				// 3 fear.
+				ctx.AddFear(3);
+				// Add 1 disease to 2 adjacent lands with invaders.
+				var x = ctx.PowerAdjacents().Where(a=>ctx.GameState.Tokens[a].HasInvaders()).Take(2).ToArray();
+				foreach(var y in x)
+					ctx.GameState.Tokens[y].Disease().Count++;
+			}
 		}
 
 	}
