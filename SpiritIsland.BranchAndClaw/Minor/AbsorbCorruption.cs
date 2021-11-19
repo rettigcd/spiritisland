f8c6a263472b6fc865ffd10763f798aba7dd3b54
Dean Rettig <rettigcd@gmail.com>
2021-11-19 06:38:27 -0500

    Absorb Corruption - make doing both optional

--- SpiritIsland.BranchAndClaw/Minor/AbsorbCorruption.cs
+++ SpiritIsland.BranchAndClaw/Minor/AbsorbCorruption.cs
@@ -11,12 +11,9 @@ namespace SpiritIsland.BranchAndClaw {
 
 			var gatherOpt = new ActionOption( "Gather 1 blight", () => ctx.Gather( 1, TokenType.Blight.Generic ) );
 			var removeOpt = new ActionOption( "Pay 1 Energy to remove 1 blight",  ()=>Pay1EnergyToRemoveBlight(ctx),  ctx.Blight>0 && 1 <= ctx.Self.Energy );
+			var doBoth = new ActionOption( "Do Both", async () => { await gatherOpt.Action(); await removeOpt.Action(); }, await ctx.YouHave("2 plant") );
 
-			if(await ctx.YouHave("2 plant" )) {
-				await gatherOpt.Action();
-				await removeOpt.Action();
-			} else
-				await ctx.SelectActionOption( gatherOpt, removeOpt );
+			await ctx.SelectActionOption( gatherOpt, removeOpt, doBoth );
 
 		}
 
