da1b3d7bcc2bc8c6f4978d27e87bd16e3bd47414
Dean Rettig <rettigcd@gmail.com>
2022-05-26 22:09:10 -0400

    2 more bug fixes + unit tests.

--- SpiritIsland.JaggedEarth/Blight/StrongEarthShattersSlowly.cs
+++ SpiritIsland.JaggedEarth/Blight/StrongEarthShattersSlowly.cs
@@ -5,8 +5,12 @@ public class StrongEarthShattersSlowly : StillHealthyBlightCard {
 	public StrongEarthShattersSlowly():base("Strong Earth Shatters Slowly",2) {}
 
 	public override ActionOption<GameState> Immediately 
-		// Each player adds 1 blight (from this card) to a land adjacent to blight.
-		=> Cmd.EachSpirit( Cmd.AddBlightedIslandBlight.To( spaceCtx => spaceCtx.AdjacentCtxs.Any( adjCtx=>adjCtx.Tokens.Blight.Any ), "land adjacent to blight" )
+		// Each player
+		=> Cmd.EachSpirit(
+			// adds 1 blight (from this card)
+			Cmd.AddBlightedIslandBlight
+				// to a land adjacent to blight.
+				.To( spaceCtx => spaceCtx.AdjacentCtxs.Any( adjCtx=>adjCtx.Tokens.Blight.Any ), "land adjacent to blight" )
 		);
 
 }
