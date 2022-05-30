91886737bfa9f2d1ea891e24c26b0c8ce6739ccc
Dean Rettig <rettigcd@gmail.com>
2022-05-30 06:36:52 -0400

    fix(Instruments of Their Own Ruin)

--- SpiritIsland.BranchAndClaw/Major/InstrumentsOfTheirOwnRuin.cs
+++ SpiritIsland.BranchAndClaw/Major/InstrumentsOfTheirOwnRuin.cs
@@ -23,24 +23,63 @@ public class InstrumentsOfTheirOwnRuin {
 	public static async Task AddStrifeThenStrifedInvadersDamageUnstrifed( TargetSpaceCtx ctx ) {
 		// add 1 strife
 		await ctx.AddStrife();
-
+		// strifed invaders deal damage to other invaders in target land.
 		await StrifedRavage.StrifedInvadersDealsDamageToOtherInvaders.Execute( ctx );
 	}
 
 
 	static Task DuringRavage_InvadersDamageInvadersInAdjacentLandsInsteadOfDahan( TargetSpaceCtx ctx ) {
-		// Note - this works regardless of them ravaging in target land or not. yay!
-		int damageFromStrifedInvaders = ctx.Tokens.InvaderTokens().OfType<HealthToken>().Where(x=>x.StrifeCount>0).Sum( si => si.FullHealth * ctx.Tokens[si] );
-		async Task Sequence( RavageAction eng ) {
-			// they damage invaders in adjacent lands instead of dahan and the land.
-			var invaderSpaceCtx = await ctx.SelectAdjacentLand( $"Apply {damageFromStrifedInvaders} damage to", x => x.HasInvaders );
-			if(invaderSpaceCtx != null)
-				await invaderSpaceCtx.DamageInvaders( damageFromStrifedInvaders );
-
-				// dahan in target land do not fight back.
-			}
-			ctx.ModifyRavage( cfg => cfg.RavageSequence = Sequence );
+		ctx.ModifyRavage( cfg => cfg.RavageSequence = eng => RavageSequence_DamageInvadersInAdjacentLand(ctx,eng) );
 		return Task.CompletedTask;
+	}
+
+	static async Task RavageSequence_DamageInvadersInAdjacentLand( TargetSpaceCtx ctx, RavageAction eng ) {
+		// Note - this works regardless of them ravaging in target land or not. yay!
+
+		// This ravage is totally different from any other.
+		// - occurs in multiple spaces
+		// - total damage is from a single centralized space
+		// - When applying damage, prefer to use up badland damage first, and save general distributed damage for other spaces.
+		// - We can't use BonusDamage Pool because that doesn't have a short-circuit to not use all of the bonus damage.
+
+		// they damage invaders in adjacent lands instead of dahan and the land.
+
+		// Get damage
+		int damageFromCenter = ctx.Tokens.InvaderTokens().OfType<HealthToken>()
+			.Where( x => x.StrifeCount > 0 )
+			.Sum( si => ctx.Tokens.AttackDamageFrom1( si ) * ctx.Tokens[si] );
+
+		// Calc Total Badlands damage available
+		var tokens = ctx.GameState.Tokens;
+		var availableBadlandDamage = ctx.Adjacent.ToDictionary(x=>x,x=>tokens[x].Badlands.Count).ToCountDict(); // captures # of badlands then sets to 0 once space is activated.
+		var activatedBadlandDamage = new CountDictionary<Space>(); // initializes when they do first damage in land, then used until depleated
+		bool HasDamage(Space space) => damageFromCenter > 0 || activatedBadlandDamage[space]>0;
+
+		var actionId = Guid.NewGuid();
+
+		// While any invaders && (damageFromStrifed>0 || )damage && any explorers
+		Space[] spaceOptions;
+		while( (spaceOptions = ctx.Adjacent.Where( space => tokens[space].HasInvaders() && HasDamage( space ) ).ToArray()).Length > 0 ) {
+			// select target invader
+			var invaderOptions = spaceOptions.SelectMany(space=>tokens[space].InvaderTokens().Select(t=>new SpaceToken(space,t))).ToArray();
+			var invader = await ctx.Decision( new Select.TokenFromManySpaces($"Instrument of Ruin Damage ({damageFromCenter}) remaining", invaderOptions,Present.Done) );
+			if(invader == null) break;
+
+			if(activatedBadlandDamage[invader.Space] > 0) {
+				// use badlands
+				activatedBadlandDamage[invader.Space]--;
+			} else {
+				// use original
+				damageFromCenter--;
+				// if badlands not activated
+				activatedBadlandDamage[invader.Space] = availableBadlandDamage[invader.Space];
+				availableBadlandDamage[invader.Space] = 0;
+			}
+
+			// apply 1 damage to selected invader
+			await ctx.GameState.Invaders.On(invader.Space, actionId ).ApplyDamageTo1(1,(HealthToken)invader.Token);
+		}
+
 	}
 
 }
