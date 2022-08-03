8ef1a2fddcdc9ebe465769021c693122a7ff8b31
Dean Rettig <rettigcd@gmail.com>
2022-08-03 10:10:11 -0400

    added test that strifed based health change doesn't change GetHashCode()

--- SpiritIsland.Tests/BranchAndClaw/Strife_Tests.cs
+++ SpiritIsland.Tests/BranchAndClaw/Strife_Tests.cs
@@ -131,6 +131,34 @@ public class Strife_Tests {
 
 	}
 
+	[Fact]
+	public void StrifedCityStillFoundAfterStrifeBasedHealthChange(){
+		// Given: dictionary contains a strifed city
+		var counts = new CountDictionary<Token>();
+		var token = new HealthToken( Invader.City, 3, 0, 1 );
+		counts[token] = 1;
+		// When: adjust health based on strife
+		HealthToken.HealthPenaltyPerStrife = 1;
+		// Then: can still find token in dictionary
+		counts.ContainsKey(token).ShouldBeTrue();
+	}
+
+	[Fact]
+	public void ChangingGetHashCode() {
+		var itemHashSet = new HashSet<Item>();
+		var k = new Item();
+
+		for(k.Id = 0; k.Id < 10; k.Id++)
+			itemHashSet.Add(k);
+
+		itemHashSet.Count.ShouldBe(1);
+	}
+
+	class Item {
+		public int Id { get; set; }
+		public override int GetHashCode() => Id;
+		public override bool Equals( object obj ) => obj is Item other && other.Id == Id;
+	}
 
 	[Fact]
 	public async Task Strife_Stops_Ravage() {
