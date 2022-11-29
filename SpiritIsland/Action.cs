2566c3e0cf143df215666fa4c70ef34d91a019a8
Dean Rettig <rettigcd@gmail.com>
2022-11-28 22:36:41 -0500

    added dictionary to Action so we can track intra-action state.

--- SpiritIsland/Action.cs
+++ SpiritIsland/Action.cs
@@ -9,5 +9,13 @@ public class UnitOfWork {
 		Id = Guid.NewGuid();
 	}
 
+	// String / object dictionary to track action things
+	public bool ContainsKey(string key) => dict != null && dict.ContainsKey( key );
+	public object this[string key]{
+		get => ContainsKey(key) ? dict[key] : throw new InvalidOperationException($"{key} was not set");
+		set => (dict??= new())[key] = value;
+	}
+	Dictionary<string, object> dict;
+
 }
 
