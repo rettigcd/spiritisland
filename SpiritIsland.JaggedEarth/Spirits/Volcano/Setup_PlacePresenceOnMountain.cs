ced0035a7e88fa024ca8e8dd5a2e2b1cf658bf47
Dean Rettig <rettigcd@gmail.com>
2022-12-14 14:42:30 -0500

    fixed Volcano init action

--- SpiritIsland.JaggedEarth/Spirits/Volcano/Setup_PlacePresenceOnMountain.cs
+++ SpiritIsland.JaggedEarth/Spirits/Volcano/Setup_PlacePresenceOnMountain.cs
@@ -2,15 +2,22 @@
 class Setup_PlacePresenceOnMountain : GrowthActionFactory { // Similar to SharpFang initialization
 
 	public override async Task ActivateAsync( SelfCtx ctx ) {
+
+		// Put 1 Presence and 1 Badlands on your starting board in a mountain of your choice.
+		// Push all Dahan from that land.
+
 		// Put 1 presence on your starting board in a mountain of your choice.
 		var options = ctx.GameState.AllActiveSpaces.Select(x=>x.Space).Where( space=>space.IsMountain );
 		var space = await ctx.Decision(new Select.Space("Add presence to",options, Present.Always));
 		await ctx.Presence.PlaceOn(space);
+		ctx.GameState.Tokens[space].Adjust( TokenType.Badlands, 1);
 
 		// Push all dahan from that land.
 		var targetCtx = ctx.Target(space);
 		if(targetCtx.Dahan.Any)
 			await targetCtx.PushDahan(targetCtx.Dahan.Count);
 	}
+
+	public override bool CouldActivateDuring( Phase speed, Spirit _ ) => speed == Phase.Init;
 
 }
