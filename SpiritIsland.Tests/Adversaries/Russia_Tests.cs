f6baee003582dfe11918fa835a9916c434bb43eb
Dean Rettig <rettigcd@gmail.com>
2023-03-04 05:31:06 -0500

    Russia Tests

--- SpiritIsland.Tests/Adversaries/Russia_Tests.cs
+++ SpiritIsland.Tests/Adversaries/Russia_Tests.cs
@@ -2,6 +2,8 @@
 
 public class Russia_Tests {
 
+	#region Level 0
+
 	[Fact]
 	public void Level0_Escalation_AddExplorersToBeastSpaces() {
 
@@ -45,7 +47,6 @@ public class Russia_Tests {
 		// All done
 		t.IsCompleted.ShouldBeTrue();
 	}
-
 
 	[Fact]
 	public void Level0_Escalation_AddExplorersToOtherBoardBeastSpaces() {
@@ -95,6 +96,10 @@ public class Russia_Tests {
 		t.IsCompleted.ShouldBeTrue();
 	}
 
+	#endregion Level 0
+
+	#region Level 1
+
 	[Fact]
 	public async Task Level1_Placement_And2Damage() {
 		// Given: Russia, Level 1
@@ -113,8 +118,12 @@ public class Russia_Tests {
 		a7.Summary.ShouldBe( "1B,1D@2" );
 	}
 
+	#endregion Level 1
+
+	#region Level 2
+
 	[Fact]
-	public void Level2_Ravage_SaveExplorer() {
+	public async Task Level2_Ravage_SaveExplorer() {
 		var cfg = Given_RussiaLevel( 2 ).SetBoards("A").SetSpirits(RiverSurges.Name);
 		GameState gameState = BuildGame( cfg );
 		Board boardA = gameState.Island.Boards[0];
@@ -124,13 +133,13 @@ public class Russia_Tests {
 		//   And: no explorers on push-destination
 		SpaceState a1 = gameState.Tokens[boardA[1]];
 		a1.Clear();
+
 		//  When: ravage
-		Task t = a5.Ravage();
 		//  Then: we push 1
-		t.IsCompleted.ShouldBeFalse();
 		Spirit spirit = gameState.Spirits[0];
-		spirit.NextDecision().HasPrompt( "Push E@1 to" ).HasOptions( "A1,A4,A6,A7,A8" ).Choose(a1.Space.Text);
-		t.IsCompleted.ShouldBeTrue();
+		await a5.Space.When_Ravaging()
+			.AwaitUser( spirit, u => u.NextDecision.HasPrompt( "Push E@1 to" ).HasOptions( "A1,A4,A6,A7,A8" ).Choose( a1.Space.Text ) )
+			.ShouldComplete("Ravage");
 
 		//   And: explorer should be on destination
 		a1.Summary.ShouldBe("1E@1");
@@ -165,6 +174,71 @@ public class Russia_Tests {
 		//   And: remaining explorers is 0
 		a3.Summary.ShouldBe( "[none]" );
 	}
+
+	[Fact]
+	public async Task Level2_DestroyingAllExplorers_Pushes1() {
+		var cfg = Given_RussiaLevel( 2 ).SetBoards( "A" ).SetSpirits( RiverSurges.Name );
+		GameState gameState = BuildGame( cfg );
+		Board boardA = gameState.Island.Boards[0];
+		Space a5 = boardA[5];
+
+		// Given: many explorers on A5
+		a5.Tokens.Given_HasTokens( "5E@1" );
+		//   And: nothing on A6
+		Space a6 = boardA[6];
+		a6.Given_ClearTokens();
+
+		// When destroying all explorers
+		await gameState.Spirits[0].When_TargetingSpace( a5, TheJungleHungers.ActAsync, ( user ) => {
+			// Then: pushes 1
+			user.NextDecision.HasPrompt( "Push E@1 to" ).HasOptions( "A1,A4,A6,A7,A8" ).Choose( "A6" );
+		} );
+		boardA[6].Tokens.Summary.ShouldBe( "1E@1" );
+	}
+
+	[Fact]
+	public async Task Level2_IsolateEliminatesDestinatoin() {
+		var cfg = Given_RussiaLevel( 2 ).SetBoards( "A" ).SetSpirits( RiverSurges.Name );
+		GameState gameState = BuildGame( cfg );
+		Board boardA = gameState.Island.Boards[0];
+		Space a5 = boardA[5];
+
+		// Given: many explorers on A5
+		a5.Tokens.Given_HasTokens( "5E@1" );
+		//   And: nothing on A6
+		Space a6 = boardA[6];
+		a6.Given_ClearTokens();
+		//   And: A7 is isolated
+		boardA[7].Tokens.Init( Token.Isolate, 1 );
+
+		// When destroying all explorers
+		await gameState.Spirits[0].When_TargetingSpace( a5, TheJungleHungers.ActAsync, ( user ) => {
+			// Then: A7 is missing from push options
+			user.NextDecision.HasPrompt( "Push E@1 to" ).HasOptions( "A1,A4,A6,A8" ).Choose( "A6" );
+		} );
+	}
+
+	[Fact]
+	public async Task Level2_IsolateTargetPreventsPush() {
+		var cfg = Given_RussiaLevel( 2 ).SetBoards( "A" ).SetSpirits( RiverSurges.Name );
+		GameState gameState = BuildGame( cfg );
+		Board boardA = gameState.Island.Boards[0];
+		Space a5 = boardA[5];
+
+		// Given: many explorers on A5
+		a5.Tokens.Given_HasTokens( "5E@1" );
+		//   And: is isolated
+		a5.Tokens.Init( Token.Isolate, 1 );
+
+		// When destroying all explorers
+		await gameState.Spirits[0].When_TargetingSpace( a5, TheJungleHungers.ActAsync, ( user ) => {
+			// Then: completes without pushing
+		} );
+	}
+
+	#endregion Level 2
+
+	#region Level 3
 
 	[Fact]
 	public void Level3_3InvadersRavage() {
@@ -217,15 +291,37 @@ public class Russia_Tests {
 		a5.Summary.ShouldBe( orig );
 	}
 
+	#endregion Level 3
 
-	// !!! Level 2 - Test/Implement - Isolate prevents pushing explorer
+	[Fact]
+	public void Level5_InvaderCardsInFearDeck() {
+		// Given: russia-5
+		GameConfiguration cfg = Given_RussiaLevel( 5 ).SetBoards( "A" ).SetSpirits( RiverSurges.Name );
+		GameState gameState = BuildGame( cfg );
+		List<InvaderCard> buildCards = gameState.InvaderDeck.Build.Cards;
+		int startingCount = buildCards.Count;
+		void ActivateCard() => gameState.Fear.AddDirect( new FearArgs( 4 ) );
+		void HasAdditionalBuilds( int additionalBuilds ) => buildCards.Count.ShouldBe( startingCount + additionalBuilds );
+		//  And: Activating 1..2 cards
+		ActivateCard(); // 1
+		ActivateCard(); // 2
 
+		// Activating 3 increments build count 0 => 1
+		HasAdditionalBuilds( 0 );
+		ActivateCard(); // 3
+		HasAdditionalBuilds( 1 );
 
-	// !!! Level 2 - Test/Implement - Isolate prevents pushing explorer
+		ActivateCard(); // 4
+		ActivateCard(); // 5
+		ActivateCard(); // 6
 
+		// Activating 7 increments build count 1 => 2
+		HasAdditionalBuilds( 1 );
+		ActivateCard(); // 7
+		HasAdditionalBuilds( 2 );
+	}
 
-	// !!! Level 5 - Test
-	// !!! Level 6 - Test
+	// Level 6 - Not testing, I will never play it.
 
 	static GameConfiguration Given_RussiaLevel( int level ) => new GameConfiguration { Adversary = new AdversaryConfig( Russia.Name, level ), ShuffleNumber = 1, };
 	static GameState BuildGame( GameConfiguration cfg ) => ConfigurableTestFixture.GameBuilder.BuildGame( cfg );
