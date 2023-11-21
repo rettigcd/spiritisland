29e2ece6cca72adae6011ad1b4c2770325043cc8
Dean Rettig <rettigcd@gmail.com>
2023-11-21 08:15:48 -0500

    Incarna-based Commands work on any Incarna (instead of specific sub-types)

--- SpiritIsland.NatureIncarnate/Spirits/BreathOfDarkness/Growth/AddOrMoveIncarnaToPresence.cs
+++ SpiritIsland.NatureIncarnate/Spirits/BreathOfDarkness/Growth/AddOrMoveIncarnaToPresence.cs
@@ -2,10 +2,10 @@
 
 public class AddOrMoveIncarnaToPresence : SpiritAction {
 
-	public AddOrMoveIncarnaToPresence():base( "AddOrMoveIncarnaToPresence" ) { }
+	public AddOrMoveIncarnaToPresence():base( "Add or Move Incarna to Presence" ) { }
 
 	public override async Task ActAsync( SelfCtx ctx ) {
-		if(ctx.Self.Presence is not BreathPresence presence) return;
+		if(ctx.Self.Presence is not IncarnaPresence presence) return;
 
 		var space = await ctx.Self.Select( new A.Space( "Select space to place Incarna.", ctx.Self.Presence.Spaces, Present.Done ) );
 		if(space == null) return;
---
+++ SpiritIsland.NatureIncarnate/Spirits/BreathOfDarkness/Growth/PushIncarna.cs
@@ -0,0 +1,14 @@
+﻿namespace SpiritIsland.NatureIncarnate;
+
+public class PushIncarna : SpiritAction {
+	public PushIncarna():base( "Push Incarna" ) { }
+
+	public override async Task ActAsync( SelfCtx ctx ) {
+		if(ctx.Self.Presence is not IncarnaPresence presence || presence.Incarna.Space == null ) return;
+
+		await TokenMover.Push(ctx.Self,presence.Incarna.Space)
+			.AddGroup(1,presence.Incarna)
+			.DoN();
+	}
+
+}
