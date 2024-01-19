bfef4ae47cc92c07a26d6b6cf4d21caf2a0448db
Dean Rettig <rettigcd@gmail.com>
2024-01-19 15:55:00 -0500

    Fixes #21 - Found no bug.  Added Unit Test to cover Terror Level 2

--- SpiritIsland.Tests/Fear/SenseOfDread_Tests.cs
+++ SpiritIsland.Tests/Fear/SenseOfDread_Tests.cs
@@ -1,4 +1,4 @@
-﻿namespace SpiritIsland.Tests;
+﻿namespace SpiritIsland.Tests.Fear;
 
 public class SenseOfDread_Tests {
 
@@ -11,7 +11,7 @@ public class SenseOfDread_Tests {
 		// Given: ravage space has 2 explorers
 		fix.InitTokens(ravageSpace, "2E@1");
 
-		// When: activeate fear card
+		// When: activate fear card
 		await new SenseOfDread().When_InvokingLevel(1, () => {
 			// And remove 1 explorer from ravage space
 			fix.Choose( ravageSpace.Text ); // select ravage space
@@ -22,6 +22,28 @@ public class SenseOfDread_Tests {
 		fix.GameState.Tokens[ravageSpace].InvaderSummary().ShouldBe("1E@1");
 
 	}
+
+	[Fact]
+	public async Task Remove1Town() {
+		var fix = new ConfigurableTestFixture();
+		var ravageSpace = fix.GameState.Island.Boards[0][5];
+		fix.InitRavageCard( ravageSpace.BuildInvaderCard() );
+
+		// Given: ravage space has 1 explorer, 1 town
+		fix.InitTokens(ravageSpace, "1E@1,1T@2");
+
+		// When: activate fear card
+		await new SenseOfDread().When_InvokingLevel(2, () => {
+			// And remove 1 explorer from ravage space
+			fix.Choose( ravageSpace.Text ); // select ravage space
+			fix.Choose( "T@2" ); // remove an explorer
+		} );
+
+		// Then: there should be 1 explorer left.
+		fix.GameState.Tokens[ravageSpace].InvaderSummary().ShouldBe("1E@1");
+
+	}
+
 
 }
 
