07cddd5e1cc6e61bd35dbcbcad1a42048eb68445
Dean Rettig <rettigcd@gmail.com>
2024-01-27 22:20:56 -0500

    closes #5 - Tests for double-blighted island effect show everything is working as expected.

--- SpiritIsland.Tests/Blight/Blight_Tests.cs
+++ SpiritIsland.Tests/Blight/Blight_Tests.cs
@@ -25,4 +25,38 @@ public class Blight_Tests {
 		gs.BlightCard.CardFlipped.ShouldBeTrue();
 
 	}
+
+	[Fact]
+	public async Task TwoBlight_TriggerBlightedIslandOnce(){
+		Spirit spirit = new LureOfTheDeepWilderness(); // any will do
+		Board board = Board.BuildBoardC();
+		GameState gs = new GameState(spirit,board);
+		gs.Initialize();
+
+		// Given: Blight Card will be "Promising Farmlands" (this card is easy to test...)
+		gs.BlightCard = new PromisingFarmlands();
+		//   And: We are on our last blight
+		BlightCard.Space.Tokens.Init(Token.Blight,1);
+
+		//   And: Next ravage is Coastal
+		gs.InvaderDeck.Ravage.Cards.Add( InvaderCard.Stage2Costal() );
+
+		//   And: Blight will Trigger on 2 Spaces.   (Town/City on C1/C2)
+		board[1].Given_InitSummary("1T@2");
+		board[2].Given_InitSummary("1C@3");
+		board[3].Given_InitSummary("");
+
+		//  When: Ravaging causes 2 blights
+		await InvaderPhase.ActAsync(gs).AwaitUser(user=>{
+
+			//  Then: "Promising Farmlands" triggers ONLY ONCE
+			user.NextDecision.HasPrompt("Island blighted").Choose("Promising Farmlands");
+			user.NextDecision.HasPrompt("Select space to Add 1 Town  Add 1 City").HasOptions("C4,C5,C6,C8").Choose("C4");
+
+		}).ShouldComplete("Invader Phase");
+
+		// And: we have blight on both 1 & 2
+		board[1].Tokens.Blight.Count.ShouldBe(1);
+		board[2].Tokens.Blight.Count.ShouldBe(1);
+	}
 }
