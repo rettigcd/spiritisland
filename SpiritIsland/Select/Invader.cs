d8b18df3066bb6889c3fae2c0919e4ac89f54cbd
Dean Rettig <rettigcd@gmail.com>
2024-06-16 21:28:08 -0400

    Rename using alias to match original type

--- SpiritIsland/Select/Invader.cs
+++ SpiritIsland/Select/Invader.cs
@@ -1,36 +1,36 @@
 ﻿namespace SpiritIsland.An;
-using Orig_Space = SpiritIsland.SpaceSpec;
-using IEnumerable_Space = IEnumerable<SpiritIsland.SpaceToken>;
+using Orig_SpaceSpec = SpiritIsland.SpaceSpec;
+using IEnumerable_SpaceToken = IEnumerable<SpiritIsland.SpaceToken>;
 
 /// <summary>
 /// Groups TokenFrom1Space that applies only to invaders
 /// </summary>
 public static class Invader {
 
-	public static A.SpaceTokenDecision ToReplace( string actionVerb, IEnumerable_Space options, Present present = Present.Always ) 
+	public static A.SpaceTokenDecision ToReplace( string actionVerb, IEnumerable_SpaceToken options, Present present = Present.Always ) 
 		=> new A.SpaceTokenDecision( $"Select invader to {actionVerb}", options, present );
 
-	public static A.SpaceTokenDecision ToRemove( IEnumerable_Space options ) 
+	public static A.SpaceTokenDecision ToRemove( IEnumerable_SpaceToken options ) 
 		=> new A.SpaceTokenDecision( "Remove invader", options, Present.Always );
 
-	public static A.SpaceTokenDecision ToRemoveByHealth( IEnumerable_Space invaders, int remainingDamage )
+	public static A.SpaceTokenDecision ToRemoveByHealth( IEnumerable_SpaceToken invaders, int remainingDamage )
 		=> new A.SpaceTokenDecision( $"Remove up to {remainingDamage} health of invaders.", invaders.Where( x => ((HumanToken)x.Token).RemainingHealth <= remainingDamage ), Present.Done ) ;
 
-	public static A.SpaceTokenDecision ForIndividualDamage(int damagePerInvader, IEnumerable_Space invaders )
+	public static A.SpaceTokenDecision ForIndividualDamage(int damagePerInvader, IEnumerable_SpaceToken invaders )
 		=> new A.SpaceTokenDecision( $"Select invader to apply {damagePerInvader} damage", invaders.Distinct(), Present.Done );
 
-	public static A.SpaceTokenDecision ForAggregateDamage( IEnumerable_Space invaderSpaces, int aggregateDamage, Present present) 
+	public static A.SpaceTokenDecision ForAggregateDamage( IEnumerable_SpaceToken invaderSpaces, int aggregateDamage, Present present) 
 		=> new A.SpaceTokenDecision( $"Damage ({aggregateDamage} remaining)", invaderSpaces, present );
 
-	public static A.SpaceTokenDecision ForAggregateDamageFromSource( Orig_Space space, HumanToken source, IToken[] invaderTokens, int aggregateDamage, Present present )
-		=> new A.SpaceTokenDecision( $"Damage from {source} ({aggregateDamage} remaining)", invaderTokens.OnScopeTokens1(space), present );
+	public static A.SpaceTokenDecision ForAggregateDamageFromSource( Orig_SpaceSpec space, HumanToken source, IToken[] invaderTokens, int aggregateDamage, Present present )
+		=> new A.SpaceTokenDecision( $"Damage from {source} ({aggregateDamage} remaining)", invaderTokens.On(space.ScopeSpace), present );
 
-	public static A.SpaceTokenDecision ForBadlandDamage(int remainingDamage, IEnumerable_Space invaders )
+	public static A.SpaceTokenDecision ForBadlandDamage(int remainingDamage, IEnumerable_SpaceToken invaders )
 		=> new A.SpaceTokenDecision( $"Select invader to apply badland damage ({remainingDamage} remaining)", invaders, Present.Done );
 
 	public static A.SpaceTokenDecision ForStrife( Space space, params ITokenClass[] groups )
 		=> new A.SpaceTokenDecision( "Add Strife", 
-			((groups!=null && 0<groups.Length) ? (IEnumerable<IToken>)space.OfAnyTag(groups) : space.InvaderTokens()).OnScopeTokens1(space.SpaceSpec),
+			((groups!=null && 0<groups.Length) ? (IEnumerable<IToken>)space.OfAnyTag(groups) : space.InvaderTokens()).On(space),
 			Present.Always 
 		);
 
