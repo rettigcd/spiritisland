5e5caccc1300a5e35b62d1da2d5e329d0d888e3a
Dean Rettig <rettigcd@gmail.com>
2024-11-09 22:14:44 -0500

    Display Innate general instructions

--- SpiritIsland.Maui/Views/SpiritPanel/InnateModel.cs
+++ SpiritIsland.Maui/Views/SpiritPanel/InnateModel.cs
@@ -4,6 +4,8 @@ namespace SpiritIsland.Maui;
 public class InnateModel(InnatePower power, Spirit spirit) : ObservableModel {
 
 	public string Title { get; } = power.Title;
+	public string Instructions { get; } = power.GeneralInstructions;
+	public bool HasInstructions { get; } = !string.IsNullOrWhiteSpace(power.GeneralInstructions);
 	public ImageSource Phase       => ImageCache.FromFile( power.DisplaySpeed.ToString().ToLower()+".png" );
 	public ImageSource SourceRange => ImageCache.FromFile("attr_" + power.RangeText.ToResourceName(".png"));
 	public ImageSource Target      => ImageCache.FromFile("attr_" + power.TargetFilter.ToResourceName(".png"));
